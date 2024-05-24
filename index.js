c53ed94b15615d1852b8c88b5cad2a361de0960c
Pedro Henrique Carvalho Ferri <pedro.ferri@db1.com.br>
2024-05-24 11:53:24 -0300

    segundo commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const html = document.querySelector('html');
+const focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.querySelector('.app__card-button--longo');
+const timer = document.getElementById('timer');
+const banner = document.querySelector('.app__image');
+// const imgCurto = document.querySelector('.app__image');
+// const imgLongo = document.querySelector('.app__image');
+
+
+
+focoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', 'imagens/foco.png');
+});
+
+curtoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', 'imagens/descanso-curto.png');
+})
+
+longoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', 'imagens/descanso-longo.png');
+})
